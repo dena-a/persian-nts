5de99bf4085ffa2b31c46b1b625987c00efd9d5e
Dena <dena.ajdari@gmail.com>
2019-08-27 13:15:24 +0430

    Composer.json added

---
+++ src/PersianNTSServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dena\PersianNTS;
+
+use Illuminate\Support\ServiceProvider;
+
+class PersianNTSServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+}
